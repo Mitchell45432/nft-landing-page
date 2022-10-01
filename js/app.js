1754090f52bf669fd6bdc283c91f4478c0300b9c
Mitchell45432 <Mitchell45432@gmail.com>
2022-10-01 16:56:19 -0400

    Updated
    Updated information for Mitchell45432 publish branch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'RanchAliensClub';
 let editions = [];
 let dots = 1;
 
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${COLLECTION_NAME} NFTs!! ⛓ Let's see how many 🔭${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} NFTs 🔬${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME} NFTs!! 🚀`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `You don't own any ${COLLECTION_NAME} NFTs 😥`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
